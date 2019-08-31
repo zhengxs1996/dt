50f8666d4b730227a04847417791784ebf209d33
undo <undo_sjm@aliyun.com>
2019-08-31 17:02:32 +0800

    权限修改数据绑定-司佳敏-2019.8.31

--- src/main/java/com/qfedu/dt/service/impl/MenuServiceImpl.java
+++ src/main/java/com/qfedu/dt/service/impl/MenuServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 public class MenuServiceImpl implements MenuService {
 
-    @Autowired
+    @Autowired(required = false)
     private MenuDao menuDao;
 
     @Override
