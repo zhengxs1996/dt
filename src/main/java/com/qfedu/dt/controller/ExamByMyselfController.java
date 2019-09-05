6c1d2ba4b6c101ce7264e2232923bd9c9f2de9a1
zhengxs <123444@aliyun.com>
2019-09-05 08:34:31 +0800

    zhengxs 20190904 学生前台参加练习考试界面相关

---
+++ src/main/java/com/qfedu/dt/controller/ExamByMyselfController.java
@@ -0,0 +1,41 @@
+package com.qfedu.dt.controller;
+
+import com.github.pagehelper.Page;
+import com.qfedu.dt.entity.ExamManage;
+import com.qfedu.dt.entity.Student;
+import com.qfedu.dt.service.ExamManageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/before")
+@CrossOrigin
+public class ExamByMyselfController {
+
+    @Autowired
+    private ExamManageService examManageService;
+
+    @RequestMapping("/findExamByMyself")
+    @ResponseBody
+    public Map<String, Object> findExamByMyself(Integer page, Integer limit, Student student) {
+        //获取学生所有报名参加的考试信息
+        int sid = student.getSid();
+//        sid = 19;
+        List<ExamManage> list = examManageService.findExamByMyself(page, limit, sid);
+        long total = ((Page) list).getTotal();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 0);
+        map.put("msg", "");
+        map.put("count", total);
+        map.put("data", list);
+        return map;
+    }
+}
