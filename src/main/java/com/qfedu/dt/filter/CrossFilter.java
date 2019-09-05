a8e1efbd63d34a1965deeed8d53b9ee8f7cff5ee
zhengxs <123444@aliyun.com>
2019-09-05 11:27:15 +0800

    zhengxs 20190904 学生前台参加练习界面相关+登录相关

---
+++ src/main/java/com/qfedu/dt/filter/CrossFilter.java
@@ -0,0 +1,32 @@
+package com.qfedu.dt.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/before/*")
+public class CrossFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) resp;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        // token name 是程序中自定义的请求头，需要在Access-Control-Allow-Headers 进行设置
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization,accept,content-type,token,name,password");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "OPTIONS,GET,POST,DELETE,PUT");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/java/com/qfedu/dt/service/impl/StudentLoginServiceImpl.java
@@ -0,0 +1,23 @@
+package com.qfedu.dt.service.impl;
+
+import com.qfedu.dt.dao.StudentLoginDao;
+import com.qfedu.dt.service.StudentLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentLoginServiceImpl implements StudentLoginService {
+
+    @Autowired
+    private StudentLoginDao studentLoginDao;
+
+    @Override
+    public void studentLogin(String name, String password) {
+        String studentPassword = studentLoginDao.findStudentPassword(name);
+        if (studentPassword == null) {
+            throw new RuntimeException("账号不存在");
+        } else if (!studentPassword.equals(password)) {
+            throw new RuntimeException("账号或者密码有误");
+        }
+    }
+}
